e7c847ea751dae61d58a2a08b5d812741bb98247
forSplinter <mugangamathys24@gmail.com>
2024-10-27 14:34:03 +0000

    uptade :wrench: keymap config

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -1,3 +1,47 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
+local keymaps = vim.keymap
+local opts = { noremap = true, silent = true }
+
+-- Increments/decrement
+keymaps.set("n", "+", "<C-a")
+keymaps.set("n", "-", "<C-x")
+
+-- Delete a word backwards
+keymaps.set("n", "dw", "vb_d")
+
+-- Select all
+keymaps.set("n", "<C-a>", "gg<S-v>G")
+
+--Jumlist
+keymaps.set("n", "<C-m>", "<C-i>", opts)
+
+-- New tab
+keymaps.set("n", "te", ":tabedit", opts)
+keymaps.set("n", "<tab>", ":tabnext<Return>", opts)
+keymaps.set("n", "<s-tab>", ":tabprev<Return>", opts)
+
+-- Split window
+
+keymaps.set("n", "ss", ":split<Return>", opts)
+keymaps.set("n", "sv", ":vsplit<Return>", opts)
+
+-- Move window
+
+keymaps.set("n", "sh", "<C-w>h")
+keymaps.set("n", "sk", "<C-w>k")
+keymaps.set("n", "sj", "<C-w>j")
+keymaps.set("n", "sl", "<C-w>l")
+
+-- Resize window
+
+keymaps.set("n", "<C-w><left>", "<C-w><")
+keymaps.set("n", "<C-w><right>", "<C-w>>")
+keymaps.set("n", "<C-w><up>", "<C-w>+")
+keymaps.set("n", "<C-w><down>", "<C-w>-")
+
+-- Diagnostics
+keymaps.set("n", "<C-j>", function()
+  vim.diagnostic.goto_next()
+end, opts)
